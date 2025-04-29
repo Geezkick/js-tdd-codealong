4032aefea56fa7b605f785e5186cf902c18b4a44
Brian <nyarienyabrian05@gmail.com>
2025-04-29 08:44:54 +0300

    Implement currentAgeForBirthYear function and pass all Jest tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,26 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    // Arrange
     const birthYear = 1984;
-    const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    const expectedAge = new Date().getFullYear() - birthYear;
+
+    // Act
+    const age = currentAgeForBirthYear(birthYear);
+
+    // Assert
+    expect(age).toBe(expectedAge);
+  });
+
+  it("returns the current year for a person born in year 0", () => {
+    // Arrange
+    const birthYear = 0;
+    const expectedAge = new Date().getFullYear();
+
+    // Act
+    const age = currentAgeForBirthYear(birthYear);
+
+    // Assert
+    expect(age).toBe(expectedAge);
   });
 });
